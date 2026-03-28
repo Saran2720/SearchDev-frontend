5c4319dae2c405567e8c4d1eb8307f63c04f92e6
Saran2720 <sarantamil2720@gmail.com>
2026-03-28 09:52:35 +0530

    test:testing for AI review

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -8,13 +8,6 @@ const App = () => {
 
       <Navbar/>
       <AppRouter/>
-      {/* <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/developers" element={<Users />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/inbox" element={<Inbox />} />
-      </Routes> */}
     </>
   )
 }
--- src/app/AppRouter.jsx
+++ src/app/AppRouter.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Login from '../features/auth/pages/Login'
 import SignUp from '../features/auth/pages/SignUp'
+import Developers from '../features/auth/pages/Developers'
 
 const AppRouter = () => {
   return (
     <Routes>
         <Route path='/login' element={<Login/>}/>
         <Route path='/SignUp' element={<SignUp/>}/>
+        <Route path='/developer' element={<Developers/>} />
     </Routes>
   )
 }
